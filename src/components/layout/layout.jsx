85ad00dfcc431472d8051646883095f600ecb9a3
DMaladyn <damian.maladyn@gmail.com>
2024-03-10 15:28:23 +0100

    Pal display src change

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,15 +2,13 @@ import classes from "./layout.module.css";
 
 import PersonalLinks from "../parts/personalLinks";
 
-/* import logo from "../public/graphics/logo.png"; */
-
 import { Link } from "react-router-dom";
 
 function Layout(props) {
   return (
     <div className={classes.layoutContainer}>
       <Link className={classes.logoLink} to="/Paldle">
-        <img className={classes.logo} src={"/logo.png"}></img>
+        <img className={classes.logo} src="/Paldle/logo.png"></img>
       </Link>
       <br></br>
 
