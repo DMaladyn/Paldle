f78f0ef17ada81954bdaf6eb16ed6beaeec5fa9c
DMaladyn <damian.maladyn@gmail.com>
2024-03-24 11:26:07 +0100

    test if preload works

--- src/components/functions/preload.js
+++ src/components/functions/preload.js
@@ -1,8 +1,8 @@
 export default function preloadIcons(names) {
   const namesToPreload = names;
 
-  namesToPreload.forEach((nameToPreload) => {
+  /* namesToPreload.forEach((nameToPreload) => {
     const img = new Image();
     img.src = `iconsResized64/${nameToPreload}_64.png`;
-  });
+  }); */
 }
